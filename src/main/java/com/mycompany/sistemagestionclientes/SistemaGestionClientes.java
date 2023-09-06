b7d8b573dc96930c9d5eb23ffef39c766d25dcca
Juan Botero <juandiegoboterorincon1994@gmail.com>
2023-09-05 23:36:53 -0500

    Crud Java

---
+++ src/main/java/com/mycompany/sistemagestionclientes/SistemaGestionClientes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemagestionclientes;
+
+/**
+ *
+ * @author JuanDiego
+ */
+public class SistemaGestionClientes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
